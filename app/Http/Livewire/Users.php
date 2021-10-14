4f57bc62fa268e1fbadbf3fc8ecdd329ae3e9034
Aristarkhus <38412229+Aristarkhus@users.noreply.github.com>
2021-10-14 14:55:21 +0700

    update user
    kurang error handling + pagination

--- app/Http/Livewire/Users.php
+++ app/Http/Livewire/Users.php
@@ -5,12 +5,13 @@ namespace App\Http\Livewire;
 use Livewire\Component;
 use App\Models\User;
 use Alert;
+use Illuminate\Support\Facades\Hash;
 
 class Users extends Component
 {
     public $users;
     public $isOpen=0;
-    public $userId, $nama, $email, $password, $role, $seksi_id;
+    public $userId, $nama, $email, $password, $konfirmasi_password, $role, $seksi_id;
     public function render()
     {
         $this->users = User::all();
@@ -41,29 +42,42 @@ class Users extends Component
             'nama' => 'required',
             'email' => 'required',
             'password' => 'required',
-        ]);
-        
-        User::updateOrCreate(['id'=>$this->userId],
-        [
-            'nama'=>$this->nama,
-            'email'=>$this->email,
-            'password'=>$this->password,
-            'role'=>$this->role,
-            'seksi_id'=>$this->seksi_id
+            'konfirmasi_password' => 'required',
+            'role' => 'required',
+            'seksi_id' => 'required'
         ]);
 
-        $this->hideModal();
-        if ($this->userId)
-            $this->emit('alert',['type'=>'success','message'=>'User Berhasil Diupdate','title'=>'Berhasil']);
-        else
-            $this->emit('alert',['type'=>'success','message'=>'User Berhasil Ditambahkan','title'=>'Berhasil']);
-        $this->userId='';
-        $this->nama='';
-        $this->email='';
-        $this->password='';
-        $this->role='';
-        $this->seksi_id='';
-        Alert::success('Berhasil','User Berhasil ditambahkan');
+        $password = $this->password;
+        $konfirmasi_password = $this->konfirmasi_password;
+
+        if($password == $konfirmasi_password) {
+        
+            User::updateOrCreate(['id' => $this->userId],
+            [
+                'nama'=>$this->nama,
+                'email'=>$this->email,
+                'password'=>Hash::make($this->password),
+                'role'=>$this->role,
+                'seksi_id'=>$this->seksi_id
+            ]);
+
+            $this->hideModal();
+            if ($this->userId)
+                $this->emit('alert',['type'=>'success','message'=>'User Berhasil Diupdate','title'=>'Berhasil']);
+            else
+                $this->emit('alert',['type'=>'success','message'=>'User Berhasil Ditambahkan','title'=>'Berhasil']);
+            $this->userId='';
+            $this->nama='';
+            $this->email='';
+            $this->password='';
+            $this->konfirmasi_password='';
+            $this->role='';
+            $this->seksi_id='';
+            Alert::success('Berhasil','User Berhasil ditambahkan');
+        
+        } else {
+            Alert::success('Gagal','Password harus sama');
+        }
            
     }
 
