9ef3375b25a03171231e09324b35bf476e057df9
Aristarkhus <38412229+Aristarkhus@users.noreply.github.com>
2021-11-09 23:17:59 +0700

    css user

--- resources/views/navigation-dropdown.blade.php
+++ resources/views/navigation-dropdown.blade.php
@@ -47,17 +47,22 @@
                 <x-jet-dropdown align="right" width="48">
                     <x-slot name="trigger">
                         @if (Laravel\Jetstream\Jetstream::managesProfilePhotos())
-                            <button class="flex text-sm border-2 border-transparent rounded-full focus:outline-none focus:border-gray-300 transition duration-150 ease-in-out">
-                                <img class="h-8 w-8 rounded-full object-cover" src="../assets/img/profile.gif" alt="{{ Auth::user()->name }}" />
+                            <button class="flex items-center text-sm font-medium text-gray-500 hover:text-gray-900 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300 transition duration-150 ease-in-out">
+                                <div>{{ Auth::user()->nama }}</div>
+
+                                <div class="ml-1">
+                                    <img class="h-8 w-8 rounded-full object-cover" src="../assets/img/profile.png" alt="{{ Auth::user()->nama }}" />
+                                </div>
                             </button>
                         @else
                             <button class="flex items-center text-sm font-medium text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300 transition duration-150 ease-in-out">
-                                <div>{{ Auth::user()->name }}</div>
+                                <div>{{ Auth::user()->nama }}</div>
 
                                 <div class="ml-1">
                                     <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
                                         <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
                                     </svg>
+                                    <img class="h-8 w-8 rounded-full object-cover" src="../assets/img/profile.png" alt="{{ Auth::user()->nama }}" />
                                 </div>
                             </button>
                         @endif
@@ -173,7 +178,7 @@
         <div class="pt-4 pb-1 border-t border-gray-200">
             <div class="flex items-center px-4">
                 <div class="flex-shrink-0">
-                    <img class="h-10 w-10 rounded-full" src="../assets/img/profile.gif" alt="{{ Auth::user()->name }}" />
+                    <img class="h-10 w-10 rounded-full" src="../assets/img/profile.png" alt="{{ Auth::user()->name }}" />
                 </div>
 
                 <div class="ml-3">
